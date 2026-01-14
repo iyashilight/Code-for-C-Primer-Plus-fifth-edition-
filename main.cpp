04ae4c170036446c1995d319cc56afb0b8852b5d
Yagam1 <yagam1@example.com>
2026-01-15 00:48:26 +0800

    C++ Primer Plus(Fifth Edition)_2.7.2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Name:yagami\n"<<"address:tokyo"<<std::endl;
+    int Dist_fur;
+    std::cout<<"please enter a distance measured in furlongs"<<std::endl;
+    std::cin>>Dist_fur;
+    std::cout<<"its "<<Dist_fur * 220<<" yards"<<std::endl;
 }
