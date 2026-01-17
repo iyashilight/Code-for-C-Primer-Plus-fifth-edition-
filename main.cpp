b02a2fc0d3e1378e70f5611f9bec8ea8e17484df
Yagam1 <yagam1@example.com>
2026-01-17 22:42:50 +0800

    C++ Primer Plus(Fifth Edition)_3.7.3

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
 
 int main() {
-    double height_inch;
-    double weight_pound;
-    constexpr double ic_mt(0.0254);
-    constexpr double pd_kg(1.0/2.2);
+    double degrees;
+    int minutes;
+    int seconds;
 
-    std::cout<<"Enter your height: ";
-    std::cin>>height_inch;
-    std::cout<<"Enter your weight: ";
-    std::cin>>weight_pound;
+    constexpr double sec_to_deg(1.0/3600.0);
+    constexpr double min_to_deg(1.0/60.0);
 
-    double height_m = height_inch * ic_mt;
-    double weight_kg = weight_pound * pd_kg;
+    std::cout<<"Enter a latitude in degrees,minutes,and seconds:\n"
+               "First,enter the degrees: ";
+    std::cin>>degrees;
 
-    double bmi = weight_kg / (height_m * height_m);
+    std::cout<<"Next,enter the minutes of arc: ";
+    std::cin>>minutes;
 
-    std::cout << "Your BMI is: " << bmi;
+    std::cout<<"Finally,enter the second of arc: ";
+    std::cin>>seconds;
+
+    double deg_sec = seconds*sec_to_deg;
+    double deg_min = minutes*min_to_deg;
+    double total_deg = degrees+deg_min+deg_sec;
+
+    std::cout<<degrees<<" degrees,"<<minutes<<" minutes,"<<seconds<<" seconds = "<<total_deg<<" degrees";
 
 }
