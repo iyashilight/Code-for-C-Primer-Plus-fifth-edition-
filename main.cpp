2d2ac504b07786ca696a38045c9e8ce0f03d6c14
Yagam1 <yagam1@example.com>
2026-01-17 23:45:15 +0800

    C++ Primer Plus(Fifth Edition)_3.7.4

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include<iostream>
 
 int main() {
-    double degrees;
-    int minutes;
-    int seconds;
+    long total_second;
 
-    constexpr double sec_to_deg(1.0/3600.0);
-    constexpr double min_to_deg(1.0/60.0);
+    std::cout<<"Enter the number of seconds: ";
+    std::cin>>total_second;
 
-    std::cout<<"Enter a latitude in degrees,minutes,and seconds:\n"
-               "First,enter the degrees: ";
-    std::cin>>degrees;
+    constexpr long second_per_day(24*3600);
+    constexpr long second_per_hour(3600);
+    constexpr long second_per_minute(60);
 
-    std::cout<<"Next,enter the minutes of arc: ";
-    std::cin>>minutes;
+    long remaining = total_second;
 
-    std::cout<<"Finally,enter the second of arc: ";
-    std::cin>>seconds;
+    long day = remaining/second_per_day;
+    long hour = remaining%second_per_day/second_per_hour;
+    long minute = remaining%second_per_hour/second_per_minute;
+    long second = remaining%second_per_minute;
 
-    double deg_sec = seconds*sec_to_deg;
-    double deg_min = minutes*min_to_deg;
-    double total_deg = degrees+deg_min+deg_sec;
-
-    std::cout<<degrees<<" degrees,"<<minutes<<" minutes,"<<seconds<<" seconds = "<<total_deg<<" degrees";
+    std::cout<<total_second<<" seconds = "<<
+        day<<" days,"<<
+            hour<<" hours,"<<
+                minute<<" minutes,"<<
+                    second<<" seconds";
 
 }
