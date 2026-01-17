1363ae1ee6cc0f0b78a5bca2d54ca1e170772f5c
Yagam1 <yagam1@example.com>
2026-01-17 22:20:10 +0800

    C++ Primer Plus(Fifth Edition)_3.7.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 
 int main() {
-    float uHeight;
-    constexpr float ConFct(1.0/12);
-    std::cout<<"Enter your Height___\b\b\b";
-    std::cin>>uHeight;
-    std::cout<<"Your height is "<<uHeight*ConFct<<" feet";
+    double height_inch;
+    double weight_pound;
+    constexpr double ic_mt(0.0254);
+    constexpr double pd_kg(1.0/2.2);
+
+    std::cout<<"Enter your height: ";
+    std::cin>>height_inch;
+    std::cout<<"Enter your weight: ";
+    std::cin>>weight_pound;
+
+    double height_m = height_inch * ic_mt;
+    double weight_kg = weight_pound * pd_kg;
+
+    double bmi = weight_kg / (height_m * height_m);
+
+    std::cout << "Your BMI is: " << bmi;
 
 }
