d88302349d73cd6353533de4284e9eb5d208dc24
Yagam1 <yagam1@example.com>
2026-01-18 00:13:06 +0800

    C++ Primer Plus(Fifth Edition)_3.7.6

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(){
 
     double distance_km;
     double fuel_liter;
+    constexpr double Europe_to_America(235.215);
 
     std::cout<<"Enter your distance in km: ";
     std::cin>>distance_km;
@@ -12,7 +13,9 @@ int main(){
     std::cin>>fuel_liter;
 
     double fuel_consumption = (fuel_liter*100.0)/distance_km;
+    double mpg = fuel_consumption*Europe_to_America;
 
-    std::cout<<"the fuel consumption per 100km is: "<<fuel_consumption;
+    std::cout<<"the fuel consumption per 100km is: "<<fuel_consumption<<std::endl;
+    std::cout<<"MPG is: "<<mpg;
 
 }
